dc88431fae9bf95f48ac61b52ac22eca8e2c1fdd
NickIsReserved <115177099+NickIsReserved@users.noreply.github.com>
2024-10-31 08:49:22 +0800

    Update serviceWorker.js

--- assets/serviceWorker.js
+++ assets/serviceWorker.js
@@ -1,1 +1,8 @@
 self.addEventListener('fetch',() => console.log("fetch"));
+var GHPATH = '/';
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/assets/index-ChONJmDo.css`,
+  `${GHPATH}/assets/index-CFXnLsP1.js`
+]
